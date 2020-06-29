492c7731cc04051d79ab62ba64e83538ecd5d461
Alaa Yahia <alaay873@live.com>
2020-06-29 14:40:57 +0200

    INDEXEDDB

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import Dexie from 'dexie';
+
 
 export default function App() {
   return (
@@ -39,7 +41,58 @@ export default function App() {
 }
 
 function Booking() {
-  return <h2>Booking Page</h2>;
+  const db = new Dexie('FormsDatabase');
+  const [formData, setFormData] = useState({ firstname: '', lastname: '' });
+
+  useEffect(
+    () => {
+      // create the store
+      db.version(1).stores({ forms: 'value' });
+    },
+    // run effect whenever the database connection changes
+    [db]
+  )
+
+  const handleSetName = fieldName => e => {
+    const value = e.target.value;
+
+    // update the state hook
+    setFormData(prevForm => ({ ...prevForm, [fieldName]: value }));
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    db.forms.put({ value: String(formData)}).then (function(){
+      //
+      // Then when data is stored, read from it
+      //
+     return ( db.forms.get( String(formData))).then((x)=> console.log(Object.keys(x.value)));
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <span>First name:</span>
+      <br />
+      <input
+        type="text"
+        name="firstname"
+        value={formData.firstname}
+        onChange={handleSetName('firstname')}
+      />
+      <br />
+      <span>Last name:</span>
+      <br />
+      <input
+        type="text"
+        name="lastname"
+        value={formData.lastname}
+        onChange={handleSetName('lastname')}
+      />
+       <br />
+      <input type="submit" value="Submit" />
+      </form>
+  )  
 }
 
 function Seating() {
